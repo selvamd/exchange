ed9d7210b5663356013c3b0469715bf382f82785
Selvam Doraisamy <selvamdoraisamy@Selvams-MacBook-Pro.local>
2025-01-18 15:20:43 -0800

    Bug fix

--- src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd /Users/selvamdoraisamy/local/Desktop/github/phoenix
-make clean 
-make
-cd /Users/selvamdoraisamy/local/Desktop/github/exchange/src
-make clean 
-make
--- src/restart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-ps -ef | grep svc | awk  '{print $2}' | xargs kill 
-sleep 2
-cd ../bin
-./loginsvc & 
-./accountsvc & 
-./gatewaysvc & 
-#say started
